7c7e9d7491c2f27bbd6d915fe3371f3d74629be5
annamolaohein <annamo@nexbe.sg>
2022-04-05 11:17:03 +0630

    Display Events

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,13 +1,23 @@
 import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
 
-export default function EventPage() {
+export default function EventPage({evt}) {
 
   // const router=useRouter()
   // console.log(router)
   return (
     <Layout>
-        <h1>My Event</h1>
+        <h1>{evt.name}</h1>
         
     </Layout>
   )
+}
+export async function getServerSideProps({query: {slug}}){
+  const res=await fetch(`${API_URL}/api/events/${slug}`)
+  const events=await res.json()
+  return{
+    props:{
+      evt:events[0]
+    }
+  }
 }
